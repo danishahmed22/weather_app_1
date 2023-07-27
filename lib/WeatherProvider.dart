7aebe9bb0517501ae9ff335d404a90e9b233f6e5
danishahmed22 <siddiquidanish973@gmail.com>
2023-07-27 17:40:17 +0530

    removed unneccessary stuff

--- lib/WeatherProvider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:weather_app_1/WeatherController.dart';
-
-final weatherControllerProvider = Provider<WeatherController>((ref) {
-  return WeatherController();
-});
